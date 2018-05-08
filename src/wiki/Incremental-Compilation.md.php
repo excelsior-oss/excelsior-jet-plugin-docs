33166d426e6fc28adabbf9c876fb7ba2c07f7baa
kit <nlipsky@excelsior-usa.com>
2018-05-08 18:36:06 +0700

    Bullets for parameters in Incremental-Compilation.md.php

--- src/wiki/Incremental-Compilation.md.php
+++ src/wiki/Incremental-Compilation.md.php
@@ -22,15 +22,15 @@ pdb {
 
 It may contain one of the following parameters:
 
-<?php param_value('keepInBuildDir', 'true'); ?> - the PDB directory will be created in the <?php target_dir('jet/build'); ?>
+* <?php param_value('keepInBuildDir', 'true'); ?> - the PDB directory will be created in the <?php target_dir('jet/build'); ?>
 directory and thus will be cleaned on every clean build. By default, the parameter is set to `false`.
 
-<?php param_pattern('baseDir', 'pdb-base-directory'); ?> - base directory for the PDB. If `keepInBuildDir` is set to `false` and `specificLocation` is not set,
+* <?php param_pattern('baseDir', 'pdb-base-directory'); ?> - base directory for the PDB. If `keepInBuildDir` is set to `false` and `specificLocation` is not set,
 the PDB directory for the current project will be located in the `groupId`/`projectName` subdirectory of `baseDir`.
 You may set the parameter either directly from the plugin configuration or using either the `jet.pdb.basedir` system property or `JETPDBBASEDIR` 
 environment variable. The default value for `baseDir` is `${user.home}/.ExcelsiorJET/PDB`.
 
-<?php param_pattern('specificLocation', 'pdb-location-directory'); ?> - in some cases, you may need to fully control the placement of the PDB.
+* <?php param_pattern('specificLocation', 'pdb-location-directory'); ?> - in some cases, you may need to fully control the placement of the PDB.
 If this parameter is set to a valid directory pathname, the compiler will place 
 the PDB for this project in that directory, possibly creating it first.
 
